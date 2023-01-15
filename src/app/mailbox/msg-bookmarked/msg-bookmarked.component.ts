1a50352aca28d981a110eecdaf86ed40b0db74fa
awa1txn <daniil.shapovalov200247@gmail.com>
2023-01-15 19:06:57 +0200

    Angular material update

--- src/app/mailbox/msg-bookmarked/msg-bookmarked.component.ts
+++ src/app/mailbox/msg-bookmarked/msg-bookmarked.component.ts
@@ -1,4 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { UserService } from 'src/app/user.service';
+
+export interface PeriodicElement {
+  username: string;
+  description: string;
+  date: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { username: 'Hydrogen', description: "Lorem Ipsum is simply dummy", date: '10/30/2022'},
+  { username: 'Helium', description: "Lorem Ipsum is simply dummy", date: '10/30/2022'},
+  { username: 'Lithium', description: "Lorem Ipsum is simply dummyLorem Ipsum is simply dummy", date: '10/30/2022'},
+  { username: 'Beryllium', description: "Lorem Ipsum is simply dummy", date: '10/30/2022'},
+  { username: 'Boron', description: "Lorem Ipsum is simply dummyLorem Ipsum is ", date: '10/30/2022'},
+  { username: 'Carbon', description: "Lorem Ipsum is simply dummy", date: '10/30/2022'},
+  { username: 'Nitrogen', description: "Lorem Ipsum is simply dummy", date: '10/30/2022'},
+  { username: 'Oxygen', description: "Lorem Ipsum is simply dummy", date: '10/30/2022'},
+  { username: 'Fluorine', description: "Lorem Ipsum is simply dummyLorem Ipsum is simply dummy", date: '10/30/2022'},
+  { username: 'Neon', description: "Lorem Ipsum is simply dummy", date: '10/30/2022'},
+];
 
 @Component({
   selector: 'app-msg-bookmarked',
@@ -6,10 +27,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./msg-bookmarked.component.scss']
 })
 export class MsgBookmarkedComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'weight', 'date'];
+  dataSource = ELEMENT_DATA;
 
-  constructor() { }
+  constructor(private us: UserService, private ac:AppComponent) { }
 
   ngOnInit(): void {
+    this.us.LoggedInNavBar()
+    if(localStorage.getItem('loggedIn') == '1'){
+    Promise.resolve().then(()=>{this.ac._$isLogged = true;})
+  }
   }
 
 }
--- src/app/mailbox/msg-send/msg-send.component.ts
+++ src/app/mailbox/msg-send/msg-send.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-msg-send',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MsgSendComponent implements OnInit {
 
-  constructor() { }
+  constructor(private us: UserService, private ac:AppComponent,) { }
 
   ngOnInit(): void {
+    this.us.LoggedInNavBar()
+    if(localStorage.getItem('loggedIn') == '1'){
+    Promise.resolve().then(()=>{this.ac._$isLogged = true;})
   }
 
+}
 }
